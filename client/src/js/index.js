bf712eaf82e3cb2df4d5628df3ae4c442759686c
yang <joey674901205@gmail.com>
2022-09-03 15:57:57 -0400

    add indexedDB

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -12,8 +12,12 @@ import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
 
+//
+import { initdb } from './database';
+
 // add images on load
 window.addEventListener('load', function () {
+  initdb();
   document.getElementById('logo').src = Logo;
   document.getElementById('bearThumbnail').src = Bear;
   document.getElementById('dogThumbnail').src = Dog;
