5054223f83eda7fb515e07914b515441c60ad1ad
yang <joey674901205@gmail.com>
2022-09-05 19:44:12 -0400

    add injectManifest

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -99,3 +99,10 @@ window.editCard = (e) => {
   // Toggles the Submit button so that it now Updates an existing contact instead of posting a new one
   submitBtnToUpdate = true;
 }
+
+if ('serviceWorker' in navigator) {
+  // use the window load event to keep the page load performant
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('./service-worker.js');
+  })
+};
